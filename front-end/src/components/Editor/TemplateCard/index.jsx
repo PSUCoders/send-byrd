7a119c2c40a9ac1d69d648bd2935c7cbad986db6
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-07-01 20:32:55 -0400

    Set up template cards

---
+++ front-end/src/components/Editor/TemplateCard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const TemplateCard = () => {
+	return (
+		<div className={styles["template-card"]}>
+			<h1>Template Title here</h1>
+		</div>
+	);
+};
+
+export default TemplateCard;
--- front-end/src/components/Editor/index.jsx
+++ front-end/src/components/Editor/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Jodit from "./Jodit";
 import sendByrdLogo from "../../assets/sendbyrd-logo.svg";
 import styles from "./styles.module.css";
+import TemplateCard from "./TemplateCard";
 
 const Editor = () => {
 	return (
@@ -9,7 +10,15 @@ const Editor = () => {
 			<div className={styles.container}>
 				<aside className={styles["left-container"]}>
 					<img src={sendByrdLogo} alt="" />
-					<h1>Editor</h1>
+					<h1>Welcome to SendByrd</h1>
+					<h3>
+						Create a custom email from scratch or pick one of our templates
+					</h3>
+					<div className={styles.templates}>
+						{[1, 2, 3, 4].map(num => {
+							return <TemplateCard key={num} />;
+						})}
+					</div>
 				</aside>
 				<aside className={styles["right-container"]}>
 					<Jodit />
